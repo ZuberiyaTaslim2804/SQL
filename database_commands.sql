6ba51d4d0e933d3bada8b9c7de8c245a45adaf57
ZUBERIYA TASLIM KHATEEB <zuberiyataslim@gmail.com>
2026-09-12 21:00:55 +0530

    Adding Database related commands

---
+++ database_commands.sql
@@ -0,0 +1,12 @@
+-- Creating a database
+CREATE DATABASE sql_db;
+
+-- Checking all databases present in server
+SHOW DATABASES;
+
+-- Selecting the database
+USE sql_db;
+
+-- Deleting a database
+DROP DATABASE sql_db;
+
